98dd852caf8439257f33b1051ced6121eb029a80
Kabidoye-17 <23365412@studentmail.ul.ie>
2025-02-28 10:47:27 +0000

    rename reference

--- src/main/java/ecosim/Environment.java
+++ src/main/java/ecosim/Environment.java
@@ -29,7 +29,7 @@ public class Environment  {
     }
 
     public void updateWeather() {
-        this.weatherManager.getRandomWeather();
+        this.weatherManager.updateRandomWeather();
 
     }
 
