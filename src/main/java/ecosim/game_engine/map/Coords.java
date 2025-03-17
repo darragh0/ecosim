cd9a670afb94c4b5c39e1fa3c6ee03bd53073387
MiaBorkoo <23391707@studentmail.ul.ie>
2025-03-17 12:22:44 +0000

    map and some

--- src/main/java/ecosim/game_engine/map/Coords.java
+++ src/main/java/ecosim/game_engine/map/Coords.java
@@ -1,28 +1,58 @@
 package ecosim.game_engine.map;
 
-
+/**
+ * Represents 2D coordinates in the ecosystem map.
+ * Stores and manages x and y positions.
+ * @author darragh0
+ */
 public class Coords {
 
     private int x;
     private int y;
 
+    /**
+     * Creates coordinates with the specified position.
+     * 
+     * @param x X-coordinate
+     * @param y Y-coordinate
+     */
     public Coords(final int x, final int y) {
         this.x = x;
         this.y = y;
     }
 
+    /**
+     * Gets the x-coordinate.
+     * 
+     * @return The x-coordinate value
+     */
     public int getX() {
         return this.x;
     }
 
+    /**
+     * Gets the y-coordinate.
+     * 
+     * @return The y-coordinate value
+     */
     public int getY() {
         return this.y;
     }
 
+    /**
+     * Sets the x-coordinate.
+     * 
+     * @param x The new x-coordinate value
+     */
     public void setX(final int x) {
         this.x = x;
     }
 
+    /**
+     * Sets the y-coordinate.
+     * 
+     * @param y The new y-coordinate value
+     */
     public void setY(final int y) {
         this.y = y;
     }
--- src/main/java/ecosim/game_engine/map/Grid.java
+++ src/main/java/ecosim/game_engine/map/Grid.java
@@ -8,25 +8,48 @@ import ecosim.game_engine.organism.Organism;
 
 /**
  * 2D grid that holds entities (animals & plants).
- *  
+ * Provides an abstraction for the spatial organization of organisms.
  * @author darragh0
  */
 public class Grid {
 
     private final Organism[][] data;
 
+    /**x
+     * Creates a new grid with the specified dimensions.
+     * 
+     * @param width Width of the grid
+     * @param height Height of the grid
+     */
     public Grid(final int width, final int height) {
         this.data = new Organism[height][width];
     }
 
+    /**
+     * Adds an organism to the grid at its current coordinates.
+     * 
+     * @param org The organism to add
+     */
     public void add(final Organism org) {
         this.data[org.getY()][org.getX()] = org;
     }
 
+    /**
+     * Removes an organism from the grid at its current coordinates.
+     * 
+     * @param org The organism to remove
+     */
     public void rmv(final Organism org) {
         this.data[org.getY()][org.getX()] = null;
     }
 
+    /**
+     * Gets the organism at the specified coordinates.
+     * 
+     * @param x X-coordinate
+     * @param y Y-coordinate
+     * @return Optional containing the organism if present, empty otherwise
+     */
     public Optional<Organism> get(final int x, final int y) {
         return Optional.ofNullable(this.data[y][x]);
     }
